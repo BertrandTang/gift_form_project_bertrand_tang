91a4ccc198b26154b2256edc9dc49da00b0e12b7
Bertrand <bertrand.tang.dev@gmail.com>
2026-04-15 17:50:36 +0200

    étape 3

---
+++ app/Http/Controllers/GiftController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gift;
+use Illuminate\Http\Request;
+
+class GiftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return inertia('giftlist', [
+            'gifts' => Gift::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('gifts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|min:3|max:50',
+            'url' => ['required', 'url'],
+            'details' => 'nullable|string',
+            'price' => 'required|decimal:0,2',
+        ]);
+        Gift::create($validated);
+        return redirect()->route('home');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gift $gift)
+    {
+        return inertia('giftshow', [
+            'gift' => $gift,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Gift $gift)
+    {
+        return view('gifts.edit', compact('gift'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Gift $gift)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|min:3|max:50',
+            'url' => ['required', 'url'],
+            'details' => 'nullable|string',
+            'price' => 'required|decimal:0,2',
+        ]);
+        $gift->update($validated);
+        return redirect()->route('home');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Gift $gift)
+    {
+        //
+    }
+}
