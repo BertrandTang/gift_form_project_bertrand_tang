8cba3ac548ab417f14c7a58293dfc770cae5a253
Bertrand <bertrand.tang.dev@gmail.com>
2026-04-16 11:56:35 +0200

    update blade

--- resources/views/gifts/create.blade.php
+++ resources/views/gifts/create.blade.php
@@ -10,33 +10,33 @@
         <div class="mb-3">
             <label for="name" class="form-label">Nom</label>
             <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
-            @error('name')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+                @error('name')
+                    <div class="text-danger small">{{ $message }}</div>
+                @enderror
         </div>
 
         <div class="mb-3">
             <label for="url" class="form-label">URL</label>
             <input type="url" class="form-control @error('url') is-invalid @enderror" id="url" name="url" value="{{ old('url') }}">
-            @error('url')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+                @error('url')
+                    <div class="text-danger small">{{ $message }}</div>
+                @enderror
         </div>
 
         <div class="mb-3">
             <label for="details" class="form-label">Détails</label>
             <textarea class="form-control @error('details') is-invalid @enderror" id="details" name="details">{{ old('details') }}</textarea>
-            @error('details')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+                @error('details')
+                    <div class="text-danger small">{{ $message }}</div>
+                @enderror
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Prix (€)</label>
             <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}" required>
-            @error('price')
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+                @error('price')
+                    <div class="text-danger small">{{ $message }}</div>
+                @enderror
         </div>
 
         <button type="submit" class="btn btn-success">Créer</button>
